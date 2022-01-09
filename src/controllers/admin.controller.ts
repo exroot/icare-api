dc198b2b9488cd580d40d96e8767c7c945184e3b
Elias Torrealba <elias@binaural.com.ve>
2022-01-09 17:46:00 -0400

    adding event logs date filters

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -31,7 +31,17 @@ export const getRecords = async (
   res: Response,
   next: NextFunction
 ) => {
-  const { page, limit, orderBy, sortBy, q, identifier, show_meta } = req.query;
+  const {
+    page,
+    limit,
+    orderBy,
+    sortBy,
+    q,
+    identifier,
+    show_meta,
+    date_start,
+    date_end,
+  } = req.query;
   console.log("identifier: ", identifier as string);
   try {
     const bitacoraService = new BitacoraService();
@@ -41,7 +51,9 @@ export const getRecords = async (
       sortBy as string,
       orderBy as string,
       q as string,
-      identifier as string
+      identifier as string,
+      date_start as string,
+      date_end as string
     );
 
     if (!bitacoraRecords) {
@@ -66,3 +78,54 @@ export const getRecords = async (
     throw err;
   }
 };
+
+// export const getRecordsFiltered = async (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   const {
+//     page,
+//     limit,
+//     orderBy,
+//     sortBy,
+//     q,
+//     identifier,
+//     show_meta,
+//     date_start,
+//     date_end,
+//   } = req.body;
+
+//   try {
+//     const bitacoraService = new BitacoraService();
+//     const bitacoraRecords = await bitacoraService.getMany(
+//       parseInt(page as string),
+//       parseInt(limit as string),
+//       sortBy as string,
+//       orderBy as string,
+//       q as string,
+//       identifier as string
+//     );
+
+//     if (!bitacoraRecords) {
+//       return res.status(200).json({
+//         status: 200,
+//         code: "Successful",
+//         message: MESSAGES.GET_MANY.SUCCESS,
+//         data: [],
+//       });
+//     }
+//     if (show_meta) {
+//       return res.status(200).json(bitacoraRecords);
+//     }
+//     return res.status(200).json({
+//       status: 200,
+//       code: "Successful",
+//       message: MESSAGES.GET_MANY.SUCCESS,
+//       data: bitacoraRecords,
+//     });
+//   } catch (err) {
+//     console.log("error: ", err);
+//     throw err;
+//   }
+// };
--- src/services/bitacora.service.ts
+++ src/services/bitacora.service.ts
@@ -1,7 +1,10 @@
-import { getRepository, Repository } from "typeorm";
+import { getRepository, Repository, Between } from "typeorm";
+import { startOfDay, endOfDay } from "date-fns";
 import { IPermission } from "../types/interfaces";
 import jwt from "jsonwebtoken";
 import { EventLog } from "../entity/EventLog";
+import { addYears, subYears } from "date-fns";
+import moment from "moment";
 
 type OrderBy = "ASC" | "DESC" | 1 | -1;
 
@@ -20,7 +23,9 @@ export class BitacoraService {
     sortBy: string,
     orderBy: string,
     q: string,
-    identifier: string
+    identifier: string,
+    date_start: string,
+    date_end: string
   ) {
     let args: any = {};
     if (q === "module") {
@@ -63,6 +68,40 @@ export class BitacoraService {
         },
         skip: page * limit - limit,
         take: limit,
+      };
+    }
+    if (date_start && date_end) {
+      console.log("start: ", date_start);
+      console.log("end: ", date_end);
+      const dateStart = moment(date_start, "DD/MM/YYYY"); // 1st argument - string, 2nd argument - format
+      const dateObjectStart = dateStart.toDate(); // convert moment.js object to Date object
+
+      const dateEnd = moment(date_end, "DD/MM/YYYY"); // 1st argument - string, 2nd argument - format
+      const dateObjectEnd = dateEnd.toDate(); // convert moment.js object to Date object
+
+      // const [day_start, month_start, year_start] = date_start.split("/");
+      // const [day_end, month_end, year_end] = date_start.split("/");
+      // const startDate = new Date(
+      //   +year_start,
+      //   parseInt(month_start) - 1,
+      //   +day_start
+      // );
+      // const endDate = new Date(+year_end, parseInt(month_end) - 1, +day_end);
+
+      const dates = Between(
+        dateObjectStart.toISOString(),
+        dateObjectEnd.toISOString()
+      );
+
+      console.log("DADA ISO: ", dateObjectStart);
+      console.log("PUPU ISO: ", dateObjectEnd);
+      console.log("BETWEEN: ", dates);
+      args = {
+        ...args,
+        where: {
+          ...args.where,
+          created_at: dates,
+        },
       };
     }
     return this._bitacoraRepo.find({ ...args, relations: this._relations });
